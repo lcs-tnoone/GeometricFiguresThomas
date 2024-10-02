519a85565d176a2a6f0f26f1c555fbfbc7837f71
Russell Gordon <rgordon@lcs.on.ca>
2024-10-02 17:53:08 -0400

    Initial Commit

---
+++ GeometricFigures/GeometricFiguresApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometricFiguresApp.swift
+//  GeometricFigures
+//
+//  Created by Russell Gordon on 2024-10-02.
+//
+
+import SwiftUI
+
+@main
+struct GeometricFiguresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
